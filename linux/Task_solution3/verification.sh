e24c8cb206dd4ce5b7a1f07907b96e4caacecded
Muntasir-Rahman-Saikat <muntasir6186@gmail.com>
2025-09-27 14:02:56 +0600

    Sakat's filtered task3

--- linux/Task_solution3/verification.sh
+++ linux/Task_solution3/verification.sh
@@ -3,12 +3,8 @@ email='muntasir6182@gmail.com'
 password='dfdgvhx7!'
 
 read -p "Type your email:" Email
-echo "Your email is:" $Email
 read -p "Type your password:" pswd
+echo
+echo "Your email is:" $Email
 echo "Your typed password is :" $pswd
 
-if [[ $email == $Email && $password == $pswd ]]; then
-    echo "Congratulations, you have successfully logged in"
-else
-    echo "invalid credentials"
-fi
